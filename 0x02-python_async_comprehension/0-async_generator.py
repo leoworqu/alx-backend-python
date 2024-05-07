8267ded17e458ea60bf667a67182f64de7dc572e
leoworqu <leoworqu@gmail.com>
2024-05-07 23:48:38 +0000

    commit 2-measure_runtime.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,8 +7,7 @@ import random
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """ The coroutine will loop 10 times, each time asynchronously wait 1 second,
-    then yield a random number between 0 and 10.  """
+    """ The coroutine will loop 10 times, each time asynchronously wait 1 second """
 
     for i in range(10):
         yield random.random()
